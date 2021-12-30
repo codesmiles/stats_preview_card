0b506b7ed4c0c05503986a43b8586f9209733aee
CodeSmiles <52099372+CodeSmiles@users.noreply.github.com>
2021-12-30 01:44:47 +0100

    UPDATE

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { useState } from "react";
 import image from "./images/image-header-desktop.jpg";
 function App() {
-  const head = `Get insights that help your business grow.`;
+
   const content = `Discover the benefits of data analytics and make better decisions
           regarding revenue, customer experience, and overall efficiency.`;
   const [blogs, setBlogs] = useState([
@@ -20,20 +20,55 @@ function App() {
         }}
       >
         <div className="row">
-          <div className="col-md-6">
-            <h1>{head}</h1>
-            <p>{content}</p>
-            <ul>
+          <div className="col-md-6 p-5 left">
+            <h1 style={{ backgroundColor: "hsl(244,38%,16%)" }}>
+              Get <span>insights</span> that help your business grow.
+            </h1>
+            <p className="mt-4" style={{ backgroundColor: "hsl(244,38%,16%)" }}>
+              {content}
+            </p>
+            <ul
+              className="row mt-5 p-0"
+              style={{
+                backgroundColor: "hsl(244,38%,16%)",
+                listStyleType: "none",
+              }}
+            >
               {blogs.map((blog) => (
-                <li key={blog.id} style={{ listStyleType: "none" }}>
-                  <h3>{blog.number}</h3>
-                  <p>{blog.value}</p>
+                <li
+                  className="col-4 mt-md-4"
+                  key={blog.id}
+                  style={{
+                    backgroundColor: "hsl(244,38%,16%)",
+                  }}
+                >
+                  <h3 style={{ backgroundColor: "hsl(244,38%,16%)" }}>
+                    {blog.number}
+                  </h3>
+                  <p style={{ backgroundColor: "hsl(244,38%,16%)" }}>
+                    {blog.value}
+                  </p>
                 </li>
               ))}
             </ul>
           </div>
-          <div className="col-md-6">
-            <img src={image} alt="" />
+          <div
+            className="col-md-6"
+            style={{
+              backgroundImage: `url(${image})`,
+              backgroundSize: "100% 100%",
+              backgroundRepeat: "no-repeat",
+              backgroundColor: "#f5f5f5",
+            }}
+          >
+            <img
+              src={image}
+              alt=""
+              style={{
+                width: "100%",
+                height: "100%",
+              }}
+            />
           </div>
         </div>
 
